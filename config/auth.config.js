4f036f0cebb080091139d98057d01dbc4aba3af9
ruslansobol <ruslansobol090@protonmail.com>
2022-03-04 05:55:00 -0800

    bug fixed

--- config/auth.config.js
+++ config/auth.config.js
@@ -6,7 +6,7 @@ module.exports = {
     /* for test */
     jwtExpiration: 6000,          // 1 minute
     jwtRefreshExpiration: 12000,  // 2 minutes
-    DOMAIN: "https://vuejs-medium.herokuapp.com",
+    BACKEND_DOMAIN: "https://vuejs-medium.herokuapp.com",
     FRONTEND_DOMAIN: "https://agitated-colden-958a3b.netlify.app"
   };
   
