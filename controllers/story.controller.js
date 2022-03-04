ffb8449acd54d619327e46dd1370d8a04caf2147
ruslansobol <ruslansobol090@protonmail.com>
2022-03-04 09:12:04 -0800

    bug fixed

--- controllers/story.controller.js
+++ controllers/story.controller.js
@@ -78,7 +78,7 @@ module.exports = {
         for (let index = 0; index < hashtags.length; index++) {
             await Hashtag.findOne({
                 where: {
-                    hashtag_name: hashtags[index].hashtag_name
+                    hashtag_name: hashtags[index]
                 }
             })
             .then(async(record) => {
@@ -91,7 +91,7 @@ module.exports = {
 
                 } else {
                     await Hashtag.create({
-                        hashtag_name: hashtags[index].hashtag_name,
+                        hashtag_name: hashtags[index],
                     })
                     .then(async newRecord=>{
                             await Stories_hashtag.create({
